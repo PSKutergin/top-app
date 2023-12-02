c2e2902c4adbdde24b8ccf564860f19b2985188a
PSKutergin <PSKutergin@gmail.com>
2023-12-02 07:20:49 +0300

    add component raiting and add hooks for him

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import { Htag, Button, P, Tag } from "../components";
+"use client";
+
+import React, { useState } from "react";
+import { Htag, Button, P, Tag, Rating } from "../components";
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
       <Htag tag="h1">Hello</Htag>
@@ -25,6 +29,7 @@ export default function Home(): JSX.Element {
       <Tag size="m" color="primary" href="#">
         Medium
       </Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
